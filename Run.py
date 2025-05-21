60094a4acf927d96c774235c6f1a5148d23bf90f
atrisorb <adrianosorbello30@gmail.com>
2025-05-21 18:09:00 +0200

    Primo commit del progetto Flask concessioni edilizie

---
+++ Run.py
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Primo commit del progetto Flask concessioni edilizie"
+git branch -M main
+git remote add origin https://github.com/atrisorb/concessioni-edilizie.git
+git push -u origin main
